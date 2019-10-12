dd320d2461794e10d3d448d03a4e7d656e6d009e
Hyungbum Kim <coolish46@gmail.com>
2019-10-12 14:54:40 -0400

    [Main] Input and Output result : pass

--- SQ_TDD/Program.cs
+++ SQ_TDD/Program.cs
@@ -10,7 +10,30 @@ namespace SQ_TDD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("First Test");
+            int lengX, lengY;
+            int angleA, angleB;
+            string userInput;
+
+            var triangle = new Triangle();
+
+            Console.Write("Input Side A length of Triangle: ");
+            userInput = Console.ReadLine();
+            lengX = Convert.ToInt32(userInput);
+            Console.Write("Input Side B length of Triangle: ");
+            userInput = Console.ReadLine();
+            lengY = Convert.ToInt32(userInput);
+
+
+            Console.WriteLine("Triangle Area is {0}.", triangle.CalcArea(lengX, lengY));
+
+            Console.Write("Input Angle A Degree of Triangle: ");
+            userInput = Console.ReadLine();
+            angleA = Convert.ToInt32(userInput);
+            Console.Write("Input Angle B Degree of Triangle: ");
+            userInput = Console.ReadLine();
+            angleB = Convert.ToInt32(userInput);
+
+            Console.WriteLine("Another Angle of Triangle is {0} degree.", triangle.CalcAngle(angleA, angleB));
         }
     }
 }
