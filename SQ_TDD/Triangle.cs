a14a9e6acdec83e41826c696fc47bb465499ca33
Hyungbum Kim <coolish46@gmail.com>
2019-10-12 12:50:45 -0400

    [Test1] CalcArea Method Test result : Fail

---
+++ SQ_TDD/Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQ_TDD
+{
+    public class Triangle
+    {
+        public Triangle()
+        {
+
+        }
+
+        public double CalcArea(int sideA, int sideB)
+        {
+            double area = 0.0;
+
+            return area;
+        }
+    }
+}
--- SQ_TDD_TEST/UnitTest1.cs
+++ SQ_TDD_TEST/UnitTest1.cs
@@ -1,14 +1,27 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace SQ_TDD_TEST
+namespace SQ_TDD.Tests
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestCalcArea()
         {
+            //Arrange
 
+            var triangle = new Triangle();
+            double exp_test_a;
+
+            //expected
+
+            exp_test_a = 50;
+
+            //Act
+            double act_test_a = triangle.CalcArea(10,10);
+
+            //Assert
+            Assert.AreEqual(exp_test_a, act_test_a);
         }
     }
 }
