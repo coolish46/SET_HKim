2f340c6fa86344b5612134b8c2fbb83a7c8a8127
Hyungbum Kim <coolish46@gmail.com>
2019-10-12 14:04:03 -0400

    [Test1] CalcArea Method Test - boundary result : Pass

--- SQ_TDD_TEST/UnitTest1.cs
+++ SQ_TDD_TEST/UnitTest1.cs
@@ -9,19 +9,30 @@ namespace SQ_TDD.Tests
         public void TestCalcArea()
         {
             //Arrange
-
             var triangle = new Triangle();
-            double exp_test_a;
+            double exp_test_CalcArea;
+
+            double exp_test_CalcAreaRang;
 
             //expected
 
-            exp_test_a = 50;
+            exp_test_CalcArea = 50;
+
+            exp_test_CalcAreaRang = 0;
 
             //Act
-            double act_test_a = triangle.CalcArea(10,10);
+            double act_test_CalcArea = triangle.CalcArea(10, 10);
+            
+            double act_test_CalcAreaRang = triangle.CalcArea(0, 10);
 
             //Assert
-            Assert.AreEqual(exp_test_a, act_test_a);
+            Assert.AreEqual(exp_test_CalcArea, act_test_CalcArea);
+
+            Assert.AreEqual(exp_test_CalcAreaRang, act_test_CalcAreaRang);
+
+
         }
     }
+
+ 
 }
