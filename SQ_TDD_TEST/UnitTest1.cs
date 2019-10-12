d712a5625dda71e53d3548ba28ac3a1adac0887e
Hyungbum Kim <coolish46@gmail.com>
2019-10-12 14:21:56 -0400

    [Test2] CalcAngle Method Test - Range result : Fail

--- SQ_TDD_TEST/UnitTest1.cs
+++ SQ_TDD_TEST/UnitTest1.cs
@@ -16,6 +16,8 @@ namespace SQ_TDD.Tests
 
             int exp_test_CalcAngle;
 
+            int exp_test_CalcAngleRange;
+
             //expected
 
             exp_test_CalcArea = 50;
@@ -24,19 +26,25 @@ namespace SQ_TDD.Tests
 
             exp_test_CalcAngle = 20;
 
+            exp_test_CalcAngleRange = 0;
+
             //Act
             double act_test_CalcArea = triangle.CalcArea(10, 10);
             
-            double act_test_CalcAreaRang = triangle.CalcArea(0, 10);
+            double act_test_CalcAreaRange = triangle.CalcArea(0, 10);
 
             int act_test_CalcAngle = triangle.CalcAngle(80,80);
+
+            int act_test_CalcAngleRange = triangle.CalcAngle(0, 80);
 
             //Assert
             Assert.AreEqual(exp_test_CalcArea, act_test_CalcArea);
 
-            Assert.AreEqual(exp_test_CalcAreaRang, act_test_CalcAreaRang);
+            Assert.AreEqual(exp_test_CalcAreaRang, act_test_CalcAreaRange);
 
             Assert.AreEqual(exp_test_CalcAngle, act_test_CalcAngle);
+
+            Assert.AreEqual(exp_test_CalcAngleRange, act_test_CalcAngleRange);
 
 
         }
